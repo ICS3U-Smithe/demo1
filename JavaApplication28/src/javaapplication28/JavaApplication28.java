54048e53b369e48c3a1d21f38620e57304326067
smitandr7141 <smitandr7141@MJ01B7F.EDU.DDSB.ADS>
2018-08-28 14:11:19 -0400

    did some stuff

--- JavaApplication28/src/javaapplication28/JavaApplication28.java
+++ JavaApplication28/src/javaapplication28/JavaApplication28.java
@@ -16,7 +16,7 @@ public class JavaApplication28 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        stuff goes here! 
+        System.out.print("Git Success! ");
         
         
         
